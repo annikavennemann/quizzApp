fc85a40defa6282f51ba0ce0a68b693c6348d082
annikavennemann <annikavennemann@aol.com>
2020-10-15 15:16:34 +0200

    edited toggle2 for show/hide answer

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,14 @@
-import toggle from './bookmark'
+import toggle1 from './bookmark'
+import toggle2 from './card-toggle-answer'
 
 document
     .querySelectorAll('.question-card__bookmark') 
     .forEach(element => {
-        element.addEventListener('click', toggle)
+        element.addEventListener('click', toggle1)
+    })
+
+document
+    .querySelectorAll('.question-card__button')
+    .forEach(element => {
+        element.addEventListener('click', toggle2)
     })
