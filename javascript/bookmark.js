6c9c917bc5aaa70194754b93b392a12736774153
annikavennemann <annikavennemann@aol.com>
2020-10-15 10:53:03 +0200

    old changes. Branch can be delited

--- javascript/bookmark.js
+++
@@ -1,7 +0,0 @@
-export default function toggle(event) {
-    event.target.parentElement
-        .querySelectorAll('.question-card__answer')
-        .forEach(element => {
-            element.classList.toggle('question-card__answer--hidden')
-        })
-}
